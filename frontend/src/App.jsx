5679c34e738612103fa94cc854ffa07b93cdc484
Kanan Niftiyev <47481598+kananniftiyev@users.noreply.github.com>
2023-11-15 22:27:44 +0100

    Not Found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import File from "./pages/File";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
@@ -23,6 +24,9 @@ function App() {
         <Route path="/auth/register" Component={Register} />
         <Route path="/home" Component={Home} />
         <Route path="/file/:uuid" Component={File} />
+
+        {/* Catch-all route for unmatched paths */}
+        <Route path="*" Component={NotFound} />
       </Routes>
     </Router>
   );
