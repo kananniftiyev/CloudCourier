6583e391832b2cf9441e77404d9a4fa6c13ac25a
Kanan Niftiyev <47481598+kananniftiyev@users.noreply.github.com>
2023-11-15 22:15:06 +0100

    Functionality
    - Added functionality to login and register forms.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Typography from "@mui/joy/Typography";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
+import Alert from "@mui/joy/Alert";
+import { motion } from "framer-motion";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [emailError, setEmailError] = useState(false);
+
+  const isFormFilled = Object.values(formData).every(
+    (value) => value.trim() !== ""
+  );
+
+  const isEmailValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    if (name === "email") {
+      const isValidEmail = isEmailValid.test(value);
+      setEmailError(!isValidEmail);
+    }
+  };
+
+  const handleSignIn = () => {
+    if (emailError) {
+      return;
+    }
+
+    console.log("Sign In Form Data:", formData);
+
+    // Perform sign-in actions here
+    // For example, send data to a server for authentication
+  };
+
   return (
     <div className="wrapper">
       <nav className="flex items-center justify-center pt-4">
@@ -26,12 +65,42 @@ function Login() {
           </div>
           <div className="max-w-xs w-full ">
             <div className="login-form-field  gap-3 items-center">
-              <Input placeholder="Email" className="field mb-4" />
-              <Input placeholder="Password" className="field" type="password" />
+              {emailError && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className="mb-4"
+                >
+                  <Alert color="danger">Please enter valid email.</Alert>
+                </motion.div>
+              )}
+              <Input
+                name="email"
+                value={formData.email}
+                error={emailError}
+                placeholder="Email"
+                className="field mb-4"
+                onChange={handleChange}
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+                value={formData.password}
+              />
             </div>
           </div>
           <div className="login-field-button max-w-xs w-full">
-            <button className=" text-white font-bold py-3 px-4 rounded-lg w-full button-in-login">
+            <button
+              className={`text-white font-bold py-3 px-4 rounded-lg w-full button-in-login ${
+                !isFormFilled || emailError ? "disabled-button" : ""
+              }`}
+              disabled={!isFormFilled || emailError}
+              onClick={handleSignIn}
+            >
               Sign In
             </button>
           </div>
