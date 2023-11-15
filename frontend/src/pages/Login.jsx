f20d19d89fd7b396ef62c54627340b9d9142cad5
Kanan Niftiyev <47481598+kananniftiyev@users.noreply.github.com>
2023-11-15 20:50:08 +0100

    Login And Register Pages.
    - Finished Login and Register Pages.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,44 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Typography from "@mui/joy/Typography";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
 
 function Login() {
-  const location = useLocation();
-
-  useEffect(() => {
-    // This function will be called whenever the route changes
-    // You can modify the body styles or perform any other actions here
-    document.body.style.backgroundColor = "white"; // Change to your desired style
-
-    return () => {
-      document.body.style.backgroundColor = ""; // Reset to default or remove the style
-    };
-  }, [location.pathname]);
+  return (
+    <div className="wrapper">
+      <nav className="flex items-center justify-center pt-4">
+        <a href="">
+          <Typography className="logo-main" level="h3">
+            CloudShare<span className="x-home">X</span>
+          </Typography>
+        </a>
+      </nav>
+      <div className="flex items-center justify-center h-screen pb-6">
+        <div className="login flex flex-col gap-6 items-center">
+          <div className="login-text-field text-center flex flex-col gap-1">
+            <Typography className="logo-main" level="h1">
+              Sign in to your account
+            </Typography>
+            <Typography className="grey" level="body-s">
+              Hello, Lets login to your account
+            </Typography>
+          </div>
+          <div className="max-w-xs w-full ">
+            <div className="login-form-field  gap-3 items-center">
+              <Input placeholder="Email" className="field mb-4" />
+              <Input placeholder="Password" className="field" type="password" />
+            </div>
+          </div>
+          <div className="login-field-button max-w-xs w-full">
+            <button className=" text-white font-bold py-3 px-4 rounded-lg w-full button-in-login">
+              Sign In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,54 @@
-function Register() {}
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Typography from "@mui/joy/Typography";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
+
+function Register() {
+  return (
+    <div className="wrapper">
+      <nav className="flex items-center justify-center pt-4">
+        <a href="">
+          <Typography className="logo-main" level="h3">
+            CloudShare<span className="x-home">X</span>
+          </Typography>
+        </a>
+      </nav>
+      <div className="flex items-center justify-center h-screen pb-6">
+        <div className="login flex flex-col gap-6 items-center">
+          <div className="login-text-field text-center flex flex-col gap-1">
+            <Typography className="logo-main" level="h1">
+              Create Your Account
+            </Typography>
+            <Typography className="grey" level="body-s">
+              Hello there! Let's create your account.
+            </Typography>
+          </div>
+          <div className="max-w-xs w-full ">
+            <div className="login-form-field gap-3 items-center">
+              <Input placeholder="Username" className="field mb-4" />
+              <Input placeholder="Email" className="field mb-4" />
+              <Input
+                placeholder="Password"
+                className="field mb-4"
+                type="password"
+              />
+              <Input
+                placeholder="Password Again"
+                className="field"
+                type="password"
+              />
+            </div>
+          </div>
+          <div className="login-field-button max-w-xs w-full">
+            <button className=" text-white font-bold py-3 px-4 rounded-lg w-full button-in-login">
+              Create my account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Register;
